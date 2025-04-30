9b096387e6acfe296ac81cf5e0b24e278bf31c0c
Samantha Perry <samnperry@arizona.edu>
2025-04-30 10:26:10 -0700

    Update __init__.py

--- octoprint_pfvs/__init__.py
+++ octoprint_pfvs/__init__.py
@@ -268,16 +268,13 @@ class PFVSPlugin(octoprint.plugin.SettingsPlugin,
             for i in range(len(light_spect_data)):
                     light_spect_data[i] = light_spect_data[i] - dark_spect_data[i]
                     
-            # r, g, b, c = self.color_sensor.color_raw  # (R, G, B)
-            # rgb = (r, g, b)
-            # color_code = self.classify_color(rgb, c)
+            r, g, b, c = self.color_sensor.color_raw
+            self._logger.info(f"r: {r}, g: {g}, b: {b}, c: {c}")
+            rgb = (r, g, b)
+            color_code = self.classify_color(rgb, c)       
             
-            # self._plugin_manager.send_plugin_message(
-                # self._identifier, 
-                # {"rgb": rgb, "predicted_color": color_code}
-            # )       
-                
-            self.predicted_material = predict_material(light_spect_data, 'R')
+            self._logger.info(f"Color: {color_code}")    
+            self.predicted_material = predict_material(light_spect_data, color_code)
             time.sleep(1)  # Adjust sampling rate
         except Exception as e:
             self._logger.error(f"Error reading spectrometer data: {e}")
@@ -288,12 +285,9 @@ class PFVSPlugin(octoprint.plugin.SettingsPlugin,
         r_norm = r / total
         g_norm = g / total
         b_norm = b / total
-        
-        self._logger.info(f"r: {r}, g: {g}, b: {b}")
-        self._logger.info(f"r_norm: {r_norm}, g_norm: {g_norm}, b_norm: {b_norm}")
 
         if c < 1000:
-            return 'K'  # Very low light, likely black
+            return 'K'
 
         if r_norm > g_norm and r_norm > b_norm:
             return 'R'
